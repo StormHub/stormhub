ec592e9ce74b007933690125b79d48587225e663
johnny <johnny.chow.19@gmail.com>
2024-11-22 11:06:05 +1000

    2024-11-18

---
+++ docs/_posts/2024-11-18-KernelMemory-Azure.markdown
@@ -0,0 +1,75 @@
+---
+layout: post
+title:  "Kernel Memory with Azure"
+date:   2024-10-10 12:01:02 +1000
+categories: .NET AI Kernel Memory
+---
+
+[Kernel Memory](https://github.com/microsoft/kernel-memory)
+
+# Azure Open AI
+On AzureOpenAI resource, deploy gpt-4 chat completion model and text-embedding-ada-002 embedding model
+```c#
+    var builder = new KernelMemoryBuilder()
+        .WithAzureOpenAITextGeneration(
+            new AzureOpenAIConfig
+            {
+                Auth = AzureOpenAIConfig.AuthTypes.APIKey,
+                APIKey = "Your AzureOpenAI api key",
+                Endpoint = "https://your-azure-open-ai-resource-name.openai.azure.com",
+                Deployment = "gpt-4"
+            })
+        .WithAzureOpenAITextEmbeddingGeneration(
+            new AzureOpenAIConfig
+            {
+                Auth = AzureOpenAIConfig.AuthTypes.APIKey,
+                APIKey = "Your AzureOpenAI api key",
+                Endpoint = "https://your-azure-open-ai-resource-name.openai.azure.com",
+                Deployment = "text-embedding-ada-002"
+            });
+```
+
+# Azure storage account 
+Azure blob storage to store kenerl memory pipeline artifacts
+```c#
+    var builder = new KernelMemoryBuilder()
+        .WithAzureBlobsDocumentStorage(
+            new AzureBlobsConfig
+            {
+                Account = "your-blob-storage-account",
+                Auth = AzureBlobsConfig.AuthTypes.AccountKey,
+                AccountKey = "your-blob-account-key",
+                Container = "document-ingestion"
+            })
+```
+
+# Azure AI Search service
+Azure AI search service as vector databases
+```c#
+    var builder = new KernelMemoryBuilder()
+        .WithAzureAISearchMemoryDb(new AzureAISearchConfig
+            {
+                Endpoint = "https://your-search-service-resource-name.search.windows.net",
+                Auth = AzureAISearchConfig.AuthTypes.APIKey,
+                APIKey = "your search service api key"
+            })
+```
+
+# Import some document and ask questions
+```c#
+    await kernelMemory.ImportDocumentAsync(
+        filePath: "resources/earth_book_2019_tagged.pdf",
+        documentId: "earth_book_2019",
+        index: "books");
+
+
+   var response =
+        await kernelMemory.AskAsync(
+            "Where is Amazon rainforest on earth?", 
+            index: "books");    
+    
+```
+Note the index name "books", kernel memory automatically creates Azure AI Search index name "books" if it does not exist and "books" folder in the blob container.
+
+[Sample code here](https://github.com/StormHub/stormhub/tree/main/resources/2024-11-18/ConsoleApp)
+
---
+++ resources/2024-11-18/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,155 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.AI;
+using Microsoft.KernelMemory.AI.OpenAI;
+using Microsoft.KernelMemory.Configuration;
+
+/*
+// Local file storage and in memory vector database for debug purposes
+using Microsoft.KernelMemory.DocumentStorage.DevTools;
+using Microsoft.KernelMemory.MemoryStorage.DevTools;
+*/
+
+IHost? host = default;
+
+try
+{
+    host = Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((builderContext, builder) =>
+        {
+            builder.AddJsonFile("appsettings.json", false);
+            builder.AddJsonFile($"appsettings.{builderContext.HostingEnvironment.EnvironmentName}.json", true);
+
+            if (builderContext.HostingEnvironment.IsDevelopment()) builder.AddUserSecrets<Program>();
+
+            builder.AddEnvironmentVariables();
+        })
+        .ConfigureServices((builderContext, services) =>
+        {
+            var textGenerationConfiguration = builderContext.Configuration
+                .GetSection("AzureOpenAITextGeneration")
+                .Get<AzureOpenAIConfig>();
+            var embeddingConfiguration = builderContext.Configuration
+                .GetSection("AzureOpenAITextEmbeddingGeneration")
+                .Get<AzureOpenAIConfig>();
+            
+            var blobStorageConfiguration = builderContext.Configuration
+                .GetSection("AzureBlobsDocumentStorage")
+                .Get<AzureBlobsConfig>()
+                ?? throw new InvalidOperationException("Azure blob storage configuration required");
+
+            var aiSearchConfiguration = builderContext.Configuration
+                .GetSection("AzureAISearch")
+                .Get<AzureAISearchConfig>()
+                ?? throw new InvalidOperationException("Azure AI search configuration required");
+
+            services.AddHttpClient(nameof(AzureOpenAIClient));
+            services.AddSingleton<ITextTokenizer, GPT4oTokenizer>();
+
+            services.AddTransient<IKernelMemoryBuilder>(provider =>
+            {
+                var factory = provider.GetRequiredService<IHttpClientFactory>();
+                var httpClient = factory.CreateClient(nameof(AzureOpenAIClient));
+
+                var textTokenizer = provider.GetRequiredService<ITextTokenizer>();
+
+                var memoryBuilder = new KernelMemoryBuilder()
+                    .WithAzureOpenAITextGeneration(
+                        textGenerationConfiguration,
+                        textTokenizer,
+                        httpClient)
+                    .WithCustomTextPartitioningOptions(
+                        new TextPartitioningOptions
+                        {
+                            MaxTokensPerLine = 128,
+                            MaxTokensPerParagraph = 512,
+                            OverlappingTokens = 32
+                        })
+                    .WithAzureOpenAITextEmbeddingGeneration(
+                        embeddingConfiguration,
+                        textTokenizer,
+                        default,
+                        false,
+                        httpClient);
+
+                /*
+                // Local file storage and in memory vector database for debug purposes
+                if (builderContext.HostingEnvironment.IsDevelopment())
+                {
+                    memoryBuilder
+                        .WithSimpleVectorDb(SimpleVectorDbConfig.Persistent)
+                        .WithSimpleFileStorage(SimpleFileStorageConfig.Persistent);
+                }
+                */
+
+                memoryBuilder
+                    .WithAzureBlobsDocumentStorage(blobStorageConfiguration)
+                    .WithAzureAISearchMemoryDb(aiSearchConfiguration);
+
+                return memoryBuilder;
+            });
+        })
+        .Build();
+
+    await host.StartAsync();
+
+    var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+
+    const string indexName = "books";
+    
+    await using (var scope = host.Services.CreateAsyncScope())
+    {
+        var builder = scope.ServiceProvider.GetRequiredService<IKernelMemoryBuilder>();
+        var kernelMemory = builder.Build();
+
+        const string documentId = "earth_book_2019";
+        if (!await kernelMemory.IsDocumentReadyAsync(documentId,
+                index: indexName,
+                cancellationToken: lifetime.ApplicationStopping))
+        {
+            Console.WriteLine("Importing memories...");
+            await kernelMemory.ImportDocumentAsync(
+               filePath: "resources/earth_book_2019_tagged.pdf",
+               documentId: documentId,
+               index: indexName,
+                cancellationToken: lifetime.ApplicationStopping);
+        }
+
+        const string question = "Where is Amazon rainforest on earth?";
+        Console.WriteLine($"Question: {question}");
+        var response =
+            await kernelMemory.AskAsync(question, index: indexName, cancellationToken: lifetime.ApplicationStopping);
+        if (response.NoResult)
+        {
+            Console.WriteLine(response.NoResultReason ?? "No answer");
+        }
+        else
+        {
+            Console.WriteLine($"Answer: \n {response.Result}");
+            Console.WriteLine("\n\nRelevant sources");
+            foreach (var citation in response.RelevantSources)
+            {
+                Console.WriteLine(citation.SourceName);
+                foreach (var partition in citation.Partitions)
+                {
+                    Console.WriteLine($" * Partition {partition.PartitionNumber}, relevance: {partition.Relevance}");
+                    Console.WriteLine(partition.Text);
+                }
+            }
+        }
+    }
+
+    lifetime.StopApplication();
+    await host.WaitForShutdownAsync(lifetime.ApplicationStopping);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Host terminated unexpectedly! \n{ex}");
+}
+finally
+{
+    host?.Dispose();
+}
